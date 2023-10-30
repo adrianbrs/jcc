d01dcf9c82d798f4d7ca5a5af4f1ddfe623bdac6
Adrian Cerbaro <adrian@cerbaro.dev>
2023-10-30 00:09:33 -0300

    docs: add example

---
+++ examples/ExemploParaTabelaDeIdentificadores.c
@@ -0,0 +1,27 @@
+* codigo fonte para testar */
+int num1;
+struct data
+{
+    int dia;
+    int mes;
+    int ano;
+} datanasc;
+
+float calcula(int p1, int *p2)
+{
+    int r;
+    r = p1 * p2;
+    return r;
+}
+int main()
+{
+    char livro[100][255];
+    int num1;
+    float num2;
+    num2 = calcula(num1, num2);
+    if (num2)
+    {
+        int p1;
+        p1 /= num2;
+    }
+}
